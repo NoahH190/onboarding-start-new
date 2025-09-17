4bd60c369d994f23aa37fc6772014248a35276d5
Noah Harman <noahharman190@gmail.com>
2025-09-16 21:48:38 -0400

    Update spi.v

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -12,7 +12,7 @@ module spi_peripheral (
     output wire [7:0] en_reg_out_15_8,
     output wire [7:0] en_reg_pwm_7_0,
     output wire [7:0] en_reg_pwm_15_8,
-    output wire [7:0] pwm_duty_cycle
+    output reg [7:0] pwm_duty_cycle
 );
 
 reg sclk_s1, sclk_s2, copi_s1, copi_s2, ncs_s1, ncs_s2; // implementtion of 2 ff synchronizer for each input
@@ -61,14 +61,12 @@ reg        in_frame;
 
 reg [15:0] en_out;
 reg [15:0] en_pwm_mode;
-reg [7:0]  pwm_duty_cycle;
 
 // Map internal regs to the split 8-bit outputs expected by top
 assign en_reg_out_7_0   = en_out[7:0];
 assign en_reg_out_15_8  = en_out[15:8];
 assign en_reg_pwm_7_0   = en_pwm_mode[7:0];
 assign en_reg_pwm_15_8  = en_pwm_mode[15:8];
-assign pwm_duty_cycle   = pwm_duty_cycle_r;
 
 wire [15:0] next_shift = {shift_reg[14:0], copi_sync};
 
@@ -95,8 +93,8 @@ always @(posedge clk or negedge rst_n) begin
             if (bit_cnt == 5'd15) begin
                 in_frame <= 1'b0;
                 
-                if (shift_reg[15]) begin
-                     case (shift_reg[14:8])
+                if (next_shift[15]) begin
+                     case (next_shift[14:8])
                         7'h00: en_out[7:0]        <= shift_reg[7:0];
                         7'h01: en_out[15:8]       <= shift_reg[7:0];
                         7'h02: en_pwm_mode[7:0]   <= shift_reg[7:0];
